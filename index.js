f0effe183655e96f609b18397eb902bd91eb53a3
Emilia <emilia.mena26@gmail.com>
2019-12-10 16:11:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function fetchBooks() {
-    return fetch("https://anapioficeandfire.com/api/books")
-        .then(resp => resp.json())
-        .then(json => renderBooks(json));
+    return fetch("https://anapioficeandfire.com/api/books") //gets data
+        .then(resp => resp.json()) //converts to JSON
+        .then(data => renderBooks(data)); //calls second function(below)
 }
 
-function renderBooks(json) {
+function renderBooks(data) {
     const main = document.querySelector('main');
-    json.forEach(book => {
+    data.forEach(book => {
         const h2 = document.createElement('h2');
         h2.innerHTML = `<h2>${book.name}</h2>`;
         main.appendChild(h2);
